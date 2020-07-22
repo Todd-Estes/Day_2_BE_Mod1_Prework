df159857ffc75850151719e12e8c5a6fe805f2e5
todd-estes <elestes@gmail.com>
2020-07-21 19:32:16 -0600

    Add Day 2 exercises and questions

---
+++ exercises/arrays.rb
@@ -0,0 +1,43 @@
+# In the below exercises, write code that achieves
+# the desired result. To check your work, run this
+# file by entering the following command in your terminal:
+# `ruby day_2/exercises/arrays.rb`
+
+# Example: Write code that stores an array in a variable,
+# then prints that array:
+animals = ["Zebra", "Giraffe", "Elephant"]
+p animals
+
+# Write code that stores an array of states in a variable,
+# then prints that array:
+states = [ "Colorado", "Georgia", "New York", "Wyoming" ]
+p states
+
+# Write code that stores an array of foods in a variable,
+# then prints that array:
+foods = [ "cheese", " pasta", "steak", "candy" ]
+p foods
+
+# Example: Write code that prints the number of elements
+# in your above array of animals:
+p animals.count
+
+# Write code that prints the number of elements
+# in your above array of foods:
+p foods.count
+
+# Write code that prints "Zebra" from your animals array:
+p animals[0]
+
+# Write code that prints the last item of your foods array:
+p foods[-1]
+
+# Write code that adds "lion" to your animals array
+# and prints the result (Hint- use a method):
+p animals << "Lion"
+p animals.push("Lion")
+
+# Write code that removes the last element from your foods array
+# and prints the result (Hint- use a method):
+p foods.pop
+p foods
---
+++ exercises/iteration.rb
@@ -0,0 +1,35 @@
+# In the below exercises, write code that achieves
+# the desired result. To check your work, run this
+# file by entering the following command in your terminal:
+# `ruby day_2/exercises/iteration.rb`
+
+# Example: Write code that iterates through a list of animals
+# and print each animal:
+animals = ["Zebra", "Giraffe", "Elephant"]
+
+animals.each do |animal|
+  p animal
+end
+
+# Write code that iterates through a list of animals and prints
+# "The <animal> is awesome!" for each animal:
+
+animals.each do |animal|
+  p "The #{animal} is awesome!"
+end
+
+# Write code that stores an array of foods in a variable,
+# then iterates over that array to print
+# "Add <food> to shopping list" for each food item:
+foods = ["apples", "bananas", "steaks", "cereal"]
+foods.each do |food|
+  p "Add #{food} to shopping list"
+end
+
+
+# Write code that stores an array of numbers in a variable,
+# then iterates over that array to print doubles of each number:
+numbers = (1..5).to_a
+numbers.each do |x|
+  p x * 2
+end
